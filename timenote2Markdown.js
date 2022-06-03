bd0973232ee97259ec01363acea00aaedf9ed442
gggxbbb <2331490629@qq.com>
2022-06-03 13:04:56 +0800

    Initial commit

---
+++ timenote2Markdown.js
@@ -0,0 +1,56 @@
+class Timenote2Markdown {
+
+    fileContent
+    notes
+
+    /**
+     *
+     * @param fileContent{Array}
+     */
+    constructor(fileContent) {
+        this.fileContent = fileContent["tables"]
+        this.notes = []
+        for (const i in this.fileContent) {
+            const d = this.fileContent[i]
+            if (d["name"] === "note") {
+                this.notes = d["data"]
+            }
+        }
+    }
+
+    /**
+     *
+     * @param target{HTMLElement}
+     */
+    showNotes(target) {
+        const notes = this.notes
+        for (const i in notes) {
+            const note = notes[i]
+            const noteElement = document.createElement("li")
+            noteElement.innerHTML = note["title"]
+            target.appendChild(noteElement)
+        }
+    }
+
+    /**
+     *
+     * @returns {Object}
+     */
+    getMarkdowns() {
+        const opt = {}
+        this.notes.forEach(note => {
+            let content = "---\n";
+            content += "title: " + note["title"] + "\n"
+            const date = new Date(note["time"] / 1000)
+            const date_str = date.toISOString()
+            const date_ymd = date_str.substring(0, 10)
+            const name = date_ymd + " " + note["title"] + ".md"
+            content += "date: " + date_str + "\n"
+            content += "---\n"
+            content += note["content"]
+            opt[name] = content
+        })
+        return opt
+    }
+
+}
